37e163938609bd4089b3b1831b7c10de87f267c8
wendeldefreitas <wendeldefreitas@hotmail.com~>
2023-08-23 19:45:12 -0300

    logica do calendario

--- interface.js
+++ interface.js
@@ -1,24 +1,71 @@
 "use strict";
 
-document.addEventListener("DOMContentLoaded", () =>{
+const DIAS_SEMANA = ["Dom", "Seg", "Ter", "Qua", "Qui", "Sex", "Sab"];
+const MESES = [
+  "Janeiro",
+  "Fevereiro",
+  "Março",
+  "Abril",
+  "Maio",
+  "Junho",
+  "Julho",
+  "Agosto",
+  "Setembro",
+  "Outubro",
+  "Novembro",
+  "Dezembro",
+];
 
-  let dataCompleta = new Date();
-  let dia = dataCompleta.getDate();
-  let mes = dataCompleta.getMonth();
-  let ano = dataCompleta.getFullYear();
-
+document.addEventListener("DOMContentLoaded", () => {
+  //VARIAVEIS
+  let dataAtual = new Date();
+  let htmlAno = document.querySelector(".ano");
+  let htmlMes = document.querySelector(".mes");
   let gridBox = document.querySelector(".grid-box");
-  
+  let setaEsquerda = document.querySelector(".seta-esquerda");
+  let setaDireita = document.querySelector(".seta-direita");
+
   //CHAMAR AS FUNÇÕES
-  add_HTML_semana_datas(gridBox);
+  addDiv(gridBox, 42);
+  setCalendario(dataAtual, htmlAno, htmlMes, gridBox);
+
+  setaEsquerda.addEventListener("click", () => {
+      dataAtual.setMonth(dataAtual.getMonth() - 1);
+      setCalendario(dataAtual, htmlAno, htmlMes, gridBox);
+  });
+  setaDireita.addEventListener("click", () => {
+      dataAtual.setMonth(dataAtual.getMonth() + 1);
+      setCalendario(dataAtual, htmlAno, htmlMes, gridBox);
+  });
 });
 
-function add_HTML_semana_datas(element){
-
-  for (let index = 0; index < 42; index++) {
+function addDiv(gridBox, qtd) {
+  for (let index = 0; index < qtd; index++) {
     let div = document.createElement("div");
-    element.appendChild(div);
+    gridBox.appendChild(div);
   }
 }
 
-//CRIAR AS FUNÇÕES DAS SETAS
+function setCalendario(data, htmlAno, htmlMes, gridBox) {
+  //variaveis
+  let index = 0;
+  //primeiro dia do mes
+  let novaData = new Date(data.getFullYear(), data.getMonth(), 1);
+  //primeiro data do calendário
+  let indexPrimeiroDia = novaData.getDay() + 7; // 7 = os dias da semana
+  novaData.setDate(novaData.getDate() - (indexPrimeiroDia - 7));
+
+  //construindo calendario
+  htmlAno.innerText = data.getFullYear();
+  htmlMes.innerText = MESES[data.getMonth()];
+
+  gridBox.childNodes.forEach((element) => {
+    if (index < 7) {
+      element.innerText = DIAS_SEMANA[index];
+    } else {
+      element.innerText = novaData.getDate();
+      novaData.setDate(novaData.getDate() + 1);
+    }
+    index++;
+  });
+}

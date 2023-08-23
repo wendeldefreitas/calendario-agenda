d9e88fc6499b8f319e2ac0adaea3ff23313b2868
wendeldefreitas <wendeldefreitas@hotmail.com~>
2023-08-22 21:10:24 -0300

    Em construção

--- interface.js
+++ interface.js
@@ -1,3 +1,23 @@
+"use strict";
+
 document.addEventListener("DOMContentLoaded", () =>{
+
+  let dataCompleta = new Date();
+  let dia = dataCompleta.getDate();
+  let mes = dataCompleta.getMonth();
+  let ano = dataCompleta.getFullYear();
+
+  let gridBox = document.querySelector(".grid-box");
   
+  //CHAMAR AS FUNÇÕES
 });
+
+function add_HTML_semana_datas(element){
+
+  for (let index = 0; index < 42; index++) {
+    //CRIAR A VARIAVEL DIV
+    element.appendChild(div);
+  }
+}
+
+//CRIAR AS FUNÇÕES DAS SETAS

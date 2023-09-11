0c153adbdcb4c1a2cbe4fbb40bc76f2c8e75e7ae
wendeldefreitas <wendeldefreitas@hotmail.com~>
2023-09-11 19:07:25 -0300

    refatorando

--- interface.js
+++ interface.js
@@ -21,57 +21,75 @@ document.addEventListener("DOMContentLoaded", () => {
   let dataAtual = new Date();
   let htmlAno = document.querySelector(".ano");
   let htmlMes = document.querySelector(".mes");
-  let gridBox = document.querySelector(".grid-box");
+  let quadrados = document.querySelector(".grid-box");
   let setaEsquerda = document.querySelector(".seta-esquerda");
   let setaDireita = document.querySelector(".seta-direita");
 
   //CHAMAR AS FUNÇÕES
-  addDiv(gridBox, 49);
-  setCalendario(dataAtual, htmlAno, htmlMes, gridBox);
+  addDiv(quadrados, 49);
+  setCalendario(dataAtual, htmlAno, htmlMes, quadrados);
 
   setaEsquerda.addEventListener("click", () => {
-      dataAtual.setMonth(dataAtual.getMonth() - 1);
-      setCalendario(dataAtual, htmlAno, htmlMes, gridBox);
+    dataAtual.setMonth(dataAtual.getMonth() - 1);
+    setCalendario(dataAtual, htmlAno, htmlMes, quadrados);
   });
   setaDireita.addEventListener("click", () => {
-      dataAtual.setMonth(dataAtual.getMonth() + 1);
-      setCalendario(dataAtual, htmlAno, htmlMes, gridBox);
+    dataAtual.setMonth(dataAtual.getMonth() + 1);
+    setCalendario(dataAtual, htmlAno, htmlMes, quadrados);
   });
+
+  clickHandleDatas(quadrados);
 });
 
-function addDiv(gridBox, qtd) {
+function addDiv(quadrados, qtd) {
   for (let index = 0; index < qtd; index++) {
     let div = document.createElement("div");
-    gridBox.appendChild(div);
+    quadrados.appendChild(div);
   }
 }
 
-function setCalendario(data, htmlAno, htmlMes, gridBox) {
-  //variaveis
+function setCalendario(data, htmlAno, htmlMes, quadrados) {
   let index = 0;
-  //primeiro dia do mes
-  let novaData = new Date(data.getFullYear(), data.getMonth(), 1);
-  //primeiro data do calendário
-  let indexPrimeiroDia = novaData.getDay() + 7; // 7 = os dias da semana
-  novaData.setDate(novaData.getDate() - (indexPrimeiroDia - 7));
-
-  //construindo calendario
+  let primeiroDiaMes = new Date(data.getFullYear(), data.getMonth(), 1);
+  let primeiroQuadrado = new Date(primeiroDiaMes);
+  primeiroQuadrado.setDate(primeiroQuadrado.getDate() - primeiroQuadrado.getDay());
   htmlAno.innerText = data.getFullYear();
   htmlMes.innerText = MESES[data.getMonth()];
 
-  gridBox.childNodes.forEach((element) => {
+  quadrados.childNodes.forEach((element) => {
     if (index < 7) {
       element.classList.add("semana");
       element.innerText = DIAS_SEMANA[index];
     } else {
       element.classList.add("data");
-      element.innerText = novaData.getDate();
-      if (novaData.getMonth() != data.getMonth()) {
+      element.innerText = primeiroQuadrado.getDate();
+
+      //deixa "APAGADO" os dias que não são do mês atual.
+      if (primeiroQuadrado.getMonth() != data.getMonth()) {
         element.classList.add("apagado");
-      }else{
+      } else {
         element.classList.remove("apagado");
       }
-      novaData.setDate(novaData.getDate() + 1);
+
+      primeiroQuadrado.setDate(primeiroQuadrado.getDate() + 1);
+    }
+    index++;
+  });
+}
+
+function clickHandleDatas(quadrados) {
+  let index = 0;
+
+  quadrados.childNodes.forEach((element) => {
+    if (index < 7) {
+      //ignorar primeira linha, dias da semana.
+    } else {
+      element.addEventListener("click", () => {
+        //Verificar a data;
+        //Buscar na planilha todas as informações queferente a essa data;
+        //Atualizar as informações no campo ".box-tarefas" do HTML;
+        console.log(getData());
+      });
     }
     index++;
   });
--- script.js
+++ script.js
@@ -1,1 +1,74 @@
 "use strict";
+const url = "https://sheetdb.io/api/v1/brwmzm3udxcln";
+
+function getData() {
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      /*CODIGO*/
+    });
+}
+
+function searchData(colum, value) {
+  return fetch(url + "/search?" + colum + "=" + value)
+    .then((response) => response.json())
+    .then((data) => {
+      /*CODIGO*/
+    });
+}
+
+function insertData() {
+  fetch(url, {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      data: [
+        {
+          id: "INCREMENT",
+          name: "Mark",
+          age: 18,
+        },
+      ],
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      /*CÓDIGO*/
+    });
+}
+
+function updateData() {
+  fetch(url, {
+    method: "PATCH",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      data: {
+        name: "Emma",
+      },
+    }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      // CÓDIGO
+    });
+}
+
+function deleteData() {
+  fetch(url, {
+    method: "DELETE",
+    headers: {
+      Accept: "application/json",
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      // CÓDIGO
+    });
+}
